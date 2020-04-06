a73cb8bd84493c49bc4bb2297fe624cffcbb0264
Angelo Deleon <rudy209@comcast.net>
2020-04-06 15:05:53 -0700

    added components to app file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
 // Old import
 // import Cards from "./components/Cards/Cards";
 // import CountyPicker from "./components/CountryPicker/CountryPicker";
 // import Chart from "./components/Chart/Chart";
 
-import { Cards, Chart, CountryPicker } from "./components";
-
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className={styles.container}>
         <Cards />
         <CountryPicker />
         <Chart />
