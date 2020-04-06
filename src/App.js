6458cb240b388154c558219acdde1cf2608f730c
Angelo Deleon <rudy209@comcast.net>
2020-04-06 13:04:12 -0700

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+// Old import
+// import Cards from "./components/Cards/Cards";
+// import CountyPicker from "./components/CountryPicker/CountryPicker";
+// import Chart from "./components/Chart/Chart";
+
+import { Cards, Chart, CountryPicker } from "./components";
+
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
